975f7fe641ef869adeffe15f5c977497d1ce5335
Hazen Hamather <hazen.hamather@rose-hulman.edu>
2016-09-30 13:01:03 -0400

    First test is a success

--- HW3/lu_pivot.m
+++ HW3/lu_pivot.m
@@ -12,16 +12,28 @@ m = size(A,1);
 n = size(A,2);
 L = zeros(m,m);
 p = 1:m;
+oldRowIndex = Inf;
 for j = 1:n-1
-%     [colMax,rowIndex] = max(abs(A(j:end,j)));
-    idx = find(p == j);
-    [colMax,rowIndex] = max(abs(A(p(idx:end),j)));
-    rowIndex = rowIndex + (j-1);
-    if rowIndex ~= j
-        ptemp = p(j);
-        p(j) = p(rowIndex);
-        p(rowIndex) = ptemp;
-        passed = 0;
+    %     [colMax,rowIndex] = max(abs(A(j:end,j)));
+    %     idx = find(p == j);
+    %     [colMax,rowIndex] = max(abs(A(p(idx:end),j)));
+    pholder = (p>=j);
+    pholder = pholder.*p;
+    %         oldRowIndex = p(rowIndex);
+    [colMax,rowIndex] = max(abs(A(p(j:end),j)));
+    if rowIndex ~= 1
+        rowIndex = rowIndex + (j-1);
+        if rowIndex ~= p(j)
+            ptemp = p(j);
+            p(j) = p(rowIndex);
+            p(rowIndex) = ptemp;
+            pidx = (p==rowIndex);
+            pidx = find(pidx);
+            passed = 0;
+        end
+    end
+    pidx = (p==rowIndex);
+    pidx = find(pidx);
 %         divisor = A(rowIndex,j);
         for i = j:m
             if colMax == 0
@@ -30,34 +42,41 @@ for j = 1:n-1
                 return;
             end
             %         mult = A(i,j)/A(j,j);
-            if i ~= p(rowIndex)
-                mult = A(i,j)/colMax;
-                A(i,:) = -mult*A(p(i),:) + A(p(rowIndex),:);
+            if p(i) ~= p(pidx)
+                mult = A(p(i),j)/colMax;
+                A(p(i),:) = -mult*A(p(pidx),:) + A(p(i),:);
                 if passed == 0
-                    L(i+1,j) = mult;
+                    L(p(i),j) = mult;
+%                     L(i+1,j) = mult;
                 else
-                    L(i,j) = mult;
+                    L(p(i),j) = mult;
+%                     L(i,j) = mult;
                 end
             else
                 passed = 1;
             end
         end
-    else
-        for i = j+1:m
-            if A(j,j) == 0
-                fprintf(2,'Divide by zero error. Exiting');
-                U = 0;
-                return;
-            end
-            %         mult = A(i,j)/A(j,j);
-            mult = A(i,j)/A(j,j);
-            A(i,:) = A(i,:) - mult*A(j,:);
-            L(i,j) = mult;
-        end
-        A = A(p,:);
         
-    end
+%     else
+%         for i = j+1:m
+%             if A(j,j) == 0
+%                 fprintf(2,'Divide by zero error. Exiting');
+%                 U = 0;
+%                 return;
+%             end
+%             %         mult = A(i,j)/A(j,j);
+%             mult = A(i,j)/A(j,j);
+%             A(i,:) = A(i,:) - mult*A(j,:);
+%             L(i,j) = mult;
+%         end
+
+%         A = A(p,:);
+        
+%     end
+    oldRowIndex = (p==rowIndex);
+    oldRowIndex = find(oldRowIndex);
 end
+L = L(p,:);
 L = L+eye(m);
 A = A(p,:);
 U = A;
