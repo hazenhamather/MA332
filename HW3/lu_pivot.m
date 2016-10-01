d9234166753974fdebd9740dc401784e4400c24e
Hazen Hamather <hazen.hamather@rose-hulman.edu>
2016-09-30 20:12:31 -0400

    Passed 9/9 tests

--- HW3/lu_pivot.m
+++ HW3/lu_pivot.m
@@ -1,83 +1,55 @@
 function [L,U,p] = lu_pivot(A)
-%function [L,U,p] = lu(A)
-%This function computes an LU factorization
-%input
-%A- matrix to be factored
-%outputs
-%L-lower triangular matrix
-%U-upper triangular matrix
-%p-permutation matrix
+%function [L,U,p] = lu_pivot(A)
+%The purpose of this function is to compute an LU factorization given an
+%A matrix
+%Input
+%A - input matrix
+%Outputs
+%L - Lower triangular matrix
+%U - Upper triangular matrix
+%p - permutation matrix
 
 m = size(A,1);
 n = size(A,2);
 L = zeros(m,m);
 p = 1:m;
-oldRowIndex = Inf;
-for j = 1:n-1
-    %     [colMax,rowIndex] = max(abs(A(j:end,j)));
-    %     idx = find(p == j);
-    %     [colMax,rowIndex] = max(abs(A(p(idx:end),j)));
-    pholder = (p>=j);
-    pholder = pholder.*p;
-    %         oldRowIndex = p(rowIndex);
-    [colMax,rowIndex] = max(abs(A(p(j:end),j)));
-    if rowIndex ~= 1
-        rowIndex = rowIndex + (j-1);
-        if rowIndex ~= p(j)
-            ptemp = p(j);
-            p(j) = p(rowIndex);
-            p(rowIndex) = ptemp;
-            pidx = (p==rowIndex);
-            pidx = find(pidx);
-            passed = 0;
-        end
+if m == 1 && n == 1
+    L = [1];
+    U = A;
+    return;
+end
+if m > n + 1
+    limit = n;
+elseif n > m+1
+    limit = m-1;
+elseif m > n
+    limit = m - 1;
+else 
+    limit = n -1;
+end
+for j = 1:limit
+    [colMax,ArowIndex] = max(abs(A(p(j:end),j)));
+    if colMax == 0
+        fprintf(2,'Denominator was zero. Had to quit.\n\n');
+        U = 0;
+        return;
     end
-    pidx = (p==rowIndex);
+    if ArowIndex ~= j
+            ArowIndex = ArowIndex + (j-1);
+            ptemp = p(j);
+            p(j) = p(ArowIndex);
+            p(ArowIndex) = ptemp;
+    end
+    pidx = (p==ArowIndex);
     pidx = find(pidx);
-%         divisor = A(rowIndex,j);
-        for i = j:m
-            if colMax == 0
-                fprintf(2,'Divide by zero error. Exiting');
-                U = 0;
-                return;
-            end
-            %         mult = A(i,j)/A(j,j);
-            if p(i) ~= p(pidx)
-                mult = A(p(i),j)/colMax;
-                A(p(i),:) = -mult*A(p(pidx),:) + A(p(i),:);
-                if passed == 0
-                    L(p(i),j) = mult;
-%                     L(i+1,j) = mult;
-                else
-                    L(p(i),j) = mult;
-%                     L(i,j) = mult;
-                end
-            else
-                passed = 1;
-            end
-        end
-        
-%     else
-%         for i = j+1:m
-%             if A(j,j) == 0
-%                 fprintf(2,'Divide by zero error. Exiting');
-%                 U = 0;
-%                 return;
-%             end
-%             %         mult = A(i,j)/A(j,j);
-%             mult = A(i,j)/A(j,j);
-%             A(i,:) = A(i,:) - mult*A(j,:);
-%             L(i,j) = mult;
-%         end
-
-%         A = A(p,:);
-        
-%     end
-    oldRowIndex = (p==rowIndex);
-    oldRowIndex = find(oldRowIndex);
+    for i = j+1:m
+        mult = A(p(i),j)/A(p(j),j);
+        A(p(i),:) = A(p(i),:) - mult*A(p(j),:);
+        L(p(i),j) = mult;
+    end
 end
 L = L(p,:);
-L = L+eye(m);
 A = A(p,:);
+L = L+eye(m);
 U = A;
 end
